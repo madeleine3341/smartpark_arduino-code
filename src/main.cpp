cb1fc29e09aab5c25cc0ac1649959605ff6a1c6f
Amine Elidrissi <elidrissiamine99@gmail.com>
2021-03-26 17:05:42 +0400

    changed server library to ESPAsyncWebServer and bug fixes related to wifi state checks

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #if defined(ESP32)
 #include <WiFi.h>
-#include <WebServer.h>
+#include "ESPAsyncWebServer.h"
 //#include <LittleFs.h>
-WebServer server(80);
+AsyncWebServer server(80);
 #elif defined(ESP8266)
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
@@ -18,17 +18,13 @@ ESP8266WebServer server(80);
 #include "FS.h"
 
 #ifndef APSSID
-#define APSSID "ESP-WIFI"
-#define APPSK  "123456789"
+#define APSSID "ESP323232"
+#define APPSK "welcome123"
 #endif
 
 const char *apssid = APSSID;
 const char *appassword = APPSK;
-bool flag1 = true;
-bool flag2 = true;
 
-const char *WIFI_SSID = wifiSSID;
-const char *WIFI_PASSWORD = wifiPassword;
 std::string FIREBASE_HOST = projectID;
 std::string API_KEY = apiKey;
 std::string USER_EMAIL = userEmail;
@@ -43,101 +39,71 @@ bool currentState = true;
 bool parkingState = true;
 int threshold = 20;
 
-void changeParkingState(bool state);
-void Setup_Firebase();
-void algorithm();
-void saveConfig();
-bool checkWifi(const char* SS_ID,const char* Password);
-bool loadConfig();
-void AP_setup();
-void sensor_setup();
+String spotPath;
 
+bool checkWifi(const char *SS_ID, const char *Password);
+// bool loadConfig();
+void setup_AP();
+void setup_firebase();
+void setup_sensor();
+void algorithm();
+void changeParkingState(bool state);
+bool isConfigured();
+void setupWifi();
+
+String ssid, password;
+bool configured = false;
 void setup()
 {
   Serial.begin(115200);
-  if(loadConfig()){
-    WiFi.mode(WIFI_STA);
+  if (!isConfigured())
+  {
+    // WiFi.mode(WIFI_STA);
     Serial.println();
     Serial.print("Configuring access point...");
-    AP_setup();
+    setup_AP();
   }
-  else{
-    Setup_Firebase();
+  else
+  {
+    setupWifi();
+    setup_firebase();
+    setup_sensor();
   }
-  
 }
 
 void loop()
 {
-  if(WiFi.status() == WL_CONNECTED){
-    if(flag1){
-      flag1 = false;
-      flag2 = true;
-      WiFi.softAPdisconnect (true);
-    }
-    algorithm();
-  }
-  else{
-    if(flag2){
-      flag1 = true;
-      flag2 = false;
-      if(WiFi.status() != WL_CONNECTED){
-        Serial.print("Waiting to make a reconnection \n");
-        for(int i =0;i<20;i++){
-          Serial.print(".");
-          delay(10);
-        }
-      }
-      if(WiFi.status() == WL_CONNECTED){
-        return;
-      }
-      AP_setup();
-    }
-    server.handleClient();
-  }
-  
-}
-
-void changeParkingState(bool state)
-{
-  SPIFFS.begin();
-  File configFile = SPIFFS.open("/config.json", "r");
-  if (!configFile) {
-    Serial.println("Failed to open config file");
-  }
-
-  size_t size = configFile.size();
-  if (size > 1024) {
-    Serial.println("Config file size is too large");
-  }
-  std::unique_ptr<char[]> buf(new char[size]);
-  configFile.readBytes(buf.get(), size);
-
-  StaticJsonDocument<200> doc;
-  auto error = deserializeJson(doc, buf.get());
-  if (error) {
-    Serial.println("Failed to parse config file");
-  }
-  String Path = doc["path"];
-  Serial.println(Path);
-  const char* P = Path.c_str();
-  
-  if (Firebase.RTDB.setBool(&fbdo, P, state))
+  if (!configured)
   {
-    //Success
-    Serial.println("Set int data success");
+    /* code */
   }
   else
   {
-    //Failed?, get the error reason from fbdo
-
-    Serial.print("Error in setBool, ");
-
-    Serial.println(fbdo.errorReason());
+    algorithm();
   }
 }
 
-void Setup_Firebase()
+void setup_sensor()
+{
+  Wire.begin(23, 19);
+  qmc.init();
+  Serial.print(" Steady State measurment");
+  for (size_t i = 0; i < 300; i++)
+  {
+    int x, y, z;
+    float a;
+
+    qmc.read(&x, &y, &z, &a);
+    init_val += x;
+    Serial.print("-");
+    delay(25);
+  }
+  init_val /= 300;
+  Serial.println(init_val);
+  delay(2000);
+}
+
+void setup_firebase()
 {
   config.host = FIREBASE_HOST;
   config.api_key = API_KEY;
@@ -147,7 +113,88 @@ void Setup_Firebase()
   Firebase.reconnectWiFi(true);
 }
 
-void algorithm(){
+bool checkWifi(const char *SS_ID, const char *Password)
+{
+  WiFi.disconnect();
+  WiFi.begin(SS_ID, Password);
+  int c = 0;
+  Serial.println("Waiting for WiFi to connect");
+  while (c < 7)
+  {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      Serial.print("Connected to wifi: ");
+      return true;
+    }
+    delay(500);
+    Serial.print("*");
+    c++;
+  }
+  Serial.print("Failed to Connect \n");
+  return false;
+}
+
+void processRequest(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
+{
+  String response = "";
+  for (size_t i = 0; i < len; i++)
+  {
+    char character = data[i];
+    response += character;
+  }
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, response);
+  JsonObject obj = doc.as<JsonObject>();
+  String pSSID = obj["ssid"];
+  String pPassword = obj["password"];
+  String pPath = obj["path"];
+  const char *charSSID = pSSID.c_str();
+  const char *charPassword = pPassword.c_str();
+  if (checkWifi(charSSID, charPassword))
+  {
+    //store to flash drive
+    spotPath = pPath;
+    Serial.print("Wifi Connected\n");
+    SPIFFS.begin();
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile)
+    {
+      Serial.println("Failed to open config file for writing\n");
+    }
+    else
+    {
+      request->send(200);
+      delay(3000);
+      configured = true;
+      serializeJson(doc, configFile);
+      Serial.print("WiFi Credential Successfully Stored in Flash\n");
+    }
+  }
+  else
+  {
+    //send a notification to the user
+    request->send(400, "text/html", "You Enter An Incorrect Password/ID");
+  }
+}
+
+void setup_AP()
+{
+  WiFi.softAP(apssid, appassword);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on(
+      "/config",
+      HTTP_POST,
+      [](AsyncWebServerRequest *request) {},
+      NULL,
+      processRequest);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void algorithm()
+{
   int x, y, z;
   float a;
 
@@ -227,109 +274,71 @@ void algorithm(){
   delay(1000);
 }
 
-void saveConfig(){
-  //Processing the Json String
-  StaticJsonDocument<200> doc;
-  String ssid1 = server.arg("plain");
-  deserializeJson(doc, ssid1);
-  JsonObject obj = doc.as<JsonObject>();
-  String SS_ID = obj["ssid"];
-  String Password = obj["password"];
-  String Path = obj["path"];
-  const char* SS = SS_ID.c_str();
-  const char* PP = Password.c_str();
-  if(checkWifi(SS,PP)){
-    //store to flash drive
-    Serial.print("Wifi Connected\n");
-    SPIFFS.begin();
-    File configFile = SPIFFS.open("/config.json", "w");
-    if (!configFile) {
-      Serial.println("Failed to open config file for writing\n");
-    }
-    else{
-    serializeJson(doc, configFile);
-    Serial.print("WiFi Credential Successfully Stored in Flash\n");
-    }
-  }
-  else{
-    //send a notification to the user
-    server.send(404,"text/html","You Enter An Incorrect Password/ID");
-  }
-}
-bool checkWifi(const char* SS_ID,const char* Password)
+void changeParkingState(bool state)
 {
-  WiFi.disconnect();
-  WiFi.begin(SS_ID,Password);
-  int c = 0;
-  Serial.println("Waiting for WiFi to connect");
-  while ( c < 20 ) {
-    if (WiFi.status() == WL_CONNECTED)
-    {
-      return true;
-    }
-    delay(500);
-    Serial.print("*");
-    c++;
+  Serial.println(spotPath);
+  if (Firebase.RTDB.setBool(&fbdo, spotPath.c_str(), state))
+  {
+    //Success
+    Serial.println("Set int data success");
   }
-  Serial.print("Failed to Connect \n");
-  return false;
+  else
+  {
+    //Failed?, get the error reason from fbdo
+
+    Serial.print("Error in setBool, ");
+
+    Serial.println(fbdo.errorReason());
+  }
 }
-bool loadConfig() {
+
+bool isConfigured()
+{
   SPIFFS.begin();
+
   File configFile = SPIFFS.open("/config.json", "r");
-  if (!configFile) {
+  if (!configFile)
+  {
     Serial.println("Failed to open config file");
+    return false;
   }
 
   size_t size = configFile.size();
-  if (size > 1024) {
+  if (size > 1024)
+  {
     Serial.println("Config file size is too large");
+    return false;
   }
+
   std::unique_ptr<char[]> buf(new char[size]);
   configFile.readBytes(buf.get(), size);
 
   StaticJsonDocument<200> doc;
-  auto error = deserializeJson(doc, buf.get());
-  if (error) {
-    Serial.println("Failed to parse config file");
-  }
 
-  String SS_ID = doc["ssid"];
-  String Password = doc["password"];
-  String Path = doc["path"];
-  const char* SS = SS_ID.c_str();
-  const char* PP = Password.c_str();
-  if(checkWifi("1",PP)){
-     return false;
-  }
-  else{
-    return true;
-  }
-}
-void AP_setup(){
-  WiFi.softAP(apssid, appassword);
-    IPAddress myIP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(myIP);
-    server.on("/config", saveConfig);
-    server.begin();
-    Serial.println("HTTP server started");
-}
-void sensor_setup(){
-  Wire.begin(23, 19); 
-  qmc.init();
-  Serial.print(" Steady State measurment");
-  for (size_t i = 0; i < 300; i++)
+  configFile.close();
+
+  DeserializationError error = deserializeJson(doc, buf.get());
+  if (error)
   {
-    int x, y, z;
-    float a;
-
-    qmc.read(&x, &y, &z, &a);
-    init_val += x;
-    Serial.print(".");
-    delay(25);
+    Serial.println("Failed to parse config file");
+    Serial.println(error.c_str());
+    return false;
   }
-  init_val /= 300;
-  Serial.println(init_val);
-  delay(2000);
+  ssid = doc["ssid"].as<String>();
+  password = doc["password"].as<String>();
+  spotPath = doc["path"].as<String>();
+  configured = doc["config"];
+  return configured;
+}
+
+void setupWifi()
+{
+  WiFi.begin(ssid.c_str(), password.c_str());
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
 }
